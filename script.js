0ed6e5ccd774d3b971946a19dbdb06acefef0ca6
ryanascherr <ryanascherr@gmail.com>
2024-03-04 16:54:32 -0500

    Added checkboxes for sorting. Added functionality for sorting alphabetically.

--- script.js
+++ script.js
@@ -49,22 +49,131 @@ const jokers = [
         hasTimesMult: false,
         image: "gluttonous_joker"
     },
-    // {
-    //     name: "",
-    //     rarity: "",
-    //     cost: 0,
-    //     effect: "",
-    //     hasChips: false,
-    //     hasPlusMult: false,
-    //     hasTimesMult: false
-    // }
+    {
+        name: "Jolly Joker",
+        rarity: "Common",
+        cost: 4,
+        effect: "<span class='mult'>+8 Mult</span> if played hand contains a Pair",
+        hasChips: false,
+        hasPlusMult: true,
+        hasTimesMult: false,
+        image: "jolly_joker"
+    },
+    {
+        name: "Zany Joker",
+        rarity: "Common",
+        cost: 4,
+        effect: "<span class='mult'>+12 Mult</span> if played hand contains a Three of a Kind",
+        hasChips: false,
+        hasPlusMult: true,
+        hasTimesMult: false,
+        image: "zany_joker"
+    },
+    {
+        name: "Mad Joker",
+        rarity: "Common",
+        cost: 4,
+        effect: "<span class='mult'>+20 Mult</span> if played hand contains a Four of a Kind",
+        hasChips: false,
+        hasPlusMult: true,
+        hasTimesMult: false,
+        image: "mad_joker"
+    },
+    {
+        name: "Crazy Joker",
+        rarity: "Common",
+        cost: 4,
+        effect: "<span class='mult'>+12 Mult</span> if played hand contains a Straight",
+        hasChips: false,
+        hasPlusMult: true,
+        hasTimesMult: false,
+        image: "crazy_joker"
+    },
+    {
+        name: "Droll Joker",
+        rarity: "Common",
+        cost: 4,
+        effect: "<span class='mult'>+10 Mult</span> if played hand contains a Flush",
+        hasChips: false,
+        hasPlusMult: true,
+        hasTimesMult: false,
+        image: "droll_joker"
+    },
+    {
+        name: "Sly Joker",
+        rarity: "Common",
+        cost: 4,
+        effect: "<span class='chips'>+50 Chips</span> if played hand contains a Pair",
+        hasChips: false,
+        hasPlusMult: true,
+        hasTimesMult: false,
+        image: "sly_joker"
+    },
+    {
+        name: "Wily Joker",
+        rarity: "Common",
+        cost: 4,
+        effect: "<span class='chips'>+100 Chips</span> if played hand contains a Three of a Kind",
+        hasChips: false,
+        hasPlusMult: true,
+        hasTimesMult: false,
+        image: "wily_joker"
+    },
+    {
+        name: "Clever Joker",
+        rarity: "Common",
+        cost: 4,
+        effect: "<span class='chips'>+150 Chips</span> if played hand contains a Four of a Kind",
+        hasChips: false,
+        hasPlusMult: true,
+        hasTimesMult: false,
+        image: "clever_joker"
+    },
+    {
+        name: "Devious Joker",
+        rarity: "Common",
+        cost: 4,
+        effect: "<span class='chips'>+100 Chips</span> if played hand contains a Straight",
+        hasChips: false,
+        hasPlusMult: true,
+        hasTimesMult: false,
+        image: "devious_joker"
+    },
+    {
+        name: "Crafty Joker",
+        rarity: "Common",
+        cost: 4,
+        effect: "<span class='chips'>+80 Chips</span> if played hand contains a Flush",
+        hasChips: false,
+        hasPlusMult: true,
+        hasTimesMult: false,
+        image: "droll_joker"
+    },
+    {
+        name: "",
+        rarity: "",
+        cost: 0,
+        effect: "",
+        hasChips: false,
+        hasPlusMult: false,
+        hasTimesMult: false,
+        image: "_joker"
+    }
 ]
+
+let sortByName = false;
 
 function init() {
     displayJokers();
 }
 
-function displayJokers() {
+function displayJokers(sortByName) {
+    jokers.sort(function(a, b) {
+        console.log(a.name);
+        console.log(b.name);
+        return a === b ? 0 : a.name < b.name ? -1 : 1;
+    });
+
     $.each(jokers, function(index, joker) { 
         $("#jokers").append(`
         <div class="joker">
@@ -77,6 +186,20 @@ function displayJokers() {
           <p><strong>Effect:</strong> ${joker.effect}</p>
         </div>`)
     });
+}
+
+$('input[type="checkbox"]').change(function() {
+    checkCheckboxes();
+    displayJokers(sortByName);
+});
+
+function checkCheckboxes() {
+    if($('#name').is(':checked')){
+        sortByName = true;
+    } else {
+        sortByName = false;
+    }
+    return sortByName;
 }
 
 init();
